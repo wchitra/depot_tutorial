27907e85c02584091fc11a58223764bc4e0842cb
Wit Chitrapongse <wchitra@gmail.com>
2011-06-28 01:55:07 -0500

    Finished D1-D3 of the Agile Web Development w/ Rails book

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,18 @@ class Product < ActiveRecord::Base
   validates :title, :uniqueness => true
   validates :image_url, :format => { :with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG, or PNG image.'}
   validates :title, :length => { :minimum => 10 }
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_items
+
+  private
+
+  def ensure_not_referenced_by_any_line_items
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
