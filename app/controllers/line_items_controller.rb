b3ed9b66aaf9b5863284c869a7f6afa979e3bb14
Wit Chitrapongse <wchitra@gmail.com>
2011-07-01 00:01:48 -0500

    Finished Task E Playtime of the Agile Web Development with Rails

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -47,7 +47,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart) }
+        format.html { redirect_to(@line_item.cart, :notice => 'Line item was successfully created.') }
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
@@ -83,7 +83,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to(line_items_url) }
+      format.html { redirect_to(current_cart, :notice => 'Item has been removed from your cart.') }
       format.xml  { head :ok }
     end
   end
